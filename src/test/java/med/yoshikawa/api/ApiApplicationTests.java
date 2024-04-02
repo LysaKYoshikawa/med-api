9287f65cfa0086eca4ec35ebedb2850103303083
Monalysa <monalysaklauck@gmail.com>
2024-04-01 23:58:32 -0300

    first commit

---
+++ src/test/java/med/yoshikawa/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package med.yoshikawa.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
