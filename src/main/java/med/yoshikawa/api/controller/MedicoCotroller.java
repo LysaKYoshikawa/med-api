225b1ff948e9b2bbf75ee16f330c3a6d2853e363
Monalysa <monalysaklauck@gmail.com>
2024-04-02 23:26:13 -0300

    feat: add controller medico

---
+++ src/main/java/med/yoshikawa/api/controller/MedicoCotroller.java
@@ -0,0 +1,16 @@
+package med.yoshikawa.api.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("medicos")
+public class MedicoCotroller {
+
+    @PostMapping
+    public void cadastrar(String json){
+        System.out.println(json);
+
+    }
+}
